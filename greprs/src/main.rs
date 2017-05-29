36db92aa487af6ebdcf84d809115e9222d989fb0
eugene-shiyanov <eugene.shiyanov@gmail.com>
2017-05-29 11:57:24 +0300

    refactoring modularity and error handling

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,20 +1,23 @@
+extern crate greprs;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use greprs::Config;
 
 fn main() {
     let args: Vec<String>  = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+    if let Err(e) = greprs::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
