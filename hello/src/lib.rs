3fb5e3d443dc5ad55fe740061583bb419da527a5
Yevhen Shyyanov <Yevhen_Shyyanov@epam.com>
2017-08-31 16:59:05 +0300

    web server. storing threads in the pool

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,8 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers : Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -8,14 +12,37 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: u32) -> ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        ThreadPool
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id));
+        }
+
+        ThreadPool {
+            workers,
+        }
     }
 
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static
     {
 
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        Worker {
+            id,
+            thread: thread::spawn(|| {}),
+        }
     }
 }
